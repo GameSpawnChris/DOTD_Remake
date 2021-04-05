4f2661e598057ceff582be38b8d408ad2b9a36b6
Chris <Hickmanc032@gmail.com>
2021-04-05 19:03:21 -0400

    Main Update to git

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerPos;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(playerPos.position.x, playerPos.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/GridMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridMovement : MonoBehaviour
+{
+    private bool isMoving;
+    private Vector3 origPos, targetPos;
+    private float timeToMove = 0.2f;
+
+    public Tilemap blockers;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && !isMoving)
+        {
+
+                StartCoroutine(MovePlayer(Vector3.up));
+            
+        }
+
+        if (Input.GetKey(KeyCode.A) && !isMoving)
+        {
+
+                StartCoroutine(MovePlayer(Vector3.left));
+            
+        }
+
+        if (Input.GetKey(KeyCode.S) && !isMoving)
+        {
+
+                StartCoroutine(MovePlayer(Vector3.down));
+            
+        }
+
+
+        if (Input.GetKey(KeyCode.D) && !isMoving)
+        {
+
+                StartCoroutine(MovePlayer(Vector3.right));
+            
+        }
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0;
+
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        Vector3Int blockerMapTile = blockers.WorldToCell(targetPos - new Vector3(0, 0.5f, 0));
+        if (blockers.GetTile(blockerMapTile) == null)
+        { 
+            while (elapsedTime < timeToMove)
+            {
+                transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+        
+            transform.position = targetPos;
+        }
+
+
+        isMoving = false;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(targetPos - new Vector3(0, 0.5f, 0), 0.2f);
+    }
+}
